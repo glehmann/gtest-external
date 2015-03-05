f692d69cc39c2ccc58df5afe4c88f4e7389995c1
Gaëtan Lehmann <gaetan.lehmann@gmail.com>
2015-03-05 18:43:35 +0100

    keep main as small as possible

--- main.cxx
+++ main.cxx
@@ -1,42 +1,11 @@
 #include "gtest/gtest.h"
 
-namespace {
-
-// The fixture for testing class Dummy.
 class DummyTest : public ::testing::Test {
- protected:
-  // You can remove any or all of the following functions if its body
-  // is empty.
-
-  DummyTest() {
-    // You can do set-up work for each test here.
-  }
-
-  virtual ~DummyTest() {
-    // You can do clean-up work that doesn't throw exceptions here.
-  }
-
-  // If the constructor and destructor are not enough for setting up
-  // and cleaning up each test, you can define the following methods:
-
-  virtual void SetUp() {
-    // Code here will be called immediately after the constructor (right
-    // before each test).
-  }
-
-  virtual void TearDown() {
-    // Code here will be called immediately after each test (right
-    // before the destructor).
-  }
-
-  // Objects declared here can be used by all tests in the test case for Dummy.
 };
 
 TEST_F(DummyTest, ZeroEqual0) {
   EXPECT_EQ(0, 0);
 }
-
-}  // namespace
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
