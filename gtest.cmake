2c446f1d465fc43f40644ca59f47ee5808527d4a
Gaëtan Lehmann <gaetan.lehmann@gmail.com>
2015-03-06 09:21:20 +0100

    use cmake 3.2's BYPRODUCTS to fix the build with Ninja generator

--- gtest.cmake
+++ gtest.cmake
@@ -1,18 +1,47 @@
 option(USE_SYSTEM_GTEST "Use system installed gtest when set to ON, or build gtest locally when set to OFF" OFF)
 
 if(USE_SYSTEM_GTEST)
-  if(${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION} VERSION_LESS 2.8)
-    message(STATUS "CMake version 2.8 or greater needed to use GTest")
-  else()
-    find_package(GTest)
-  endif()
+  find_package(GTest)
 else()
   include(ExternalProject)
+
+  # the binary dir must be know before creating the external project in order
+  # to pass the byproducts
+  set(prefix "${CMAKE_CURRENT_BINARY_DIR}/gtest-external")
+  set(binary_dir "${prefix}/src/gtest-external-build")
+
+  set(byproducts)
+  foreach(lib gtest gtest_main)
+    if(CMAKE_CONFIGURATION_TYPES)
+      set(${lib}_properties)
+      foreach(type ${CMAKE_CONFIGURATION_TYPES})
+        string(TOUPPER ${type} TYPE)
+        set(file ${binary_dir}/${type}/${CMAKE_STATIC_LIBRARY_PREFIX}${lib}${CMAKE_STATIC_LIBRARY_SUFFIX})
+        list(APPEND ${lib}_properties IMPORTED_LOCATION_${TYPE} ${file})
+        list(APPEND byproducts ${file})
+      endforeach()
+    else()
+      set(file ${binary_dir}/${CMAKE_STATIC_LIBRARY_PREFIX}${lib}${CMAKE_STATIC_LIBRARY_SUFFIX})
+      set(${lib}_properties IMPORTED_LOCATION ${file})
+      list(APPEND byproducts ${file})
+    endif()
+  endforeach()
+
+  set(build_byproducts)
+  if(${CMAKE_VERSION} VERSION_LESS 3.2)
+    if(CMAKE_GENERATOR MATCHES "Ninja")
+      message(WARNING "Building GTest with Ninja has known issues with CMake older than 3.2")
+    endif()
+  else()
+    set(build_byproducts BUILD_BYPRODUCTS ${byproducts})
+  endif()
 
   ExternalProject_Add(
     gtest-external
     URL http://googletest.googlecode.com/files/gtest-1.7.0.zip
     URL_MD5 2d6ec8ccdf5c46b05ba54a9fd1d130d7
+    PREFIX ${prefix}
+    BINARY_DIR ${binary_dir}
     CMAKE_CACHE_ARGS
       -DCMAKE_CXX_COMPILER:FILEPATH=${CMAKE_CXX_COMPILER}
       -DCMAKE_CXX_FLAGS:STRING=${CMAKE_CXX_FLAGS}
@@ -28,22 +57,13 @@ else()
       -DCMAKE_C_FLAGS_RELWITHDEBINFO:STRING=${CMAKE_C_FLAGS_RELWITHDEBINFO}
       -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
       -Dgtest_force_shared_crt:BOOL=ON
+    ${build_byproducts}
     INSTALL_COMMAND "")
 
-  ExternalProject_Get_Property(gtest-external binary_dir)
   foreach(lib gtest gtest_main)
     add_library(${lib} IMPORTED STATIC)
     add_dependencies(${lib} gtest-external)
-    if(CMAKE_CONFIGURATION_TYPES)
-      foreach(type ${CMAKE_CONFIGURATION_TYPES})
-        string(TOUPPER ${type} TYPE)
-        set_target_properties(${lib} PROPERTIES
-          IMPORTED_LOCATION_${TYPE} ${binary_dir}/${type}/${CMAKE_STATIC_LIBRARY_PREFIX}${lib}${CMAKE_STATIC_LIBRARY_SUFFIX})
-      endforeach()
-    else()
-      set_target_properties(${lib} PROPERTIES
-        IMPORTED_LOCATION ${binary_dir}/${CMAKE_STATIC_LIBRARY_PREFIX}${lib}${CMAKE_STATIC_LIBRARY_SUFFIX})
-    endif()
+    set_target_properties(${lib} PROPERTIES ${${lib}_properties})
   endforeach()
 
   ExternalProject_Get_Property(gtest-external source_dir)
